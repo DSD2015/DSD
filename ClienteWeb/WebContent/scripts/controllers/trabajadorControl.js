65b1907411c9999fa4e8a37adc2da762ee511351
joselynrojas91 <joselynrojas91@gmail.com>
2015-08-08 03:14:56 -0500

    UI - Backend changes

--- ClienteWeb/WebContent/scripts/controllers/trabajadorControl.js
+++ ClienteWeb/WebContent/scripts/controllers/trabajadorControl.js
@@ -1,4 +1,4 @@
-app.controller('TrabajadorController', function($scope, $http) {
+app.controller('TrabajadorController', function($scope, $http, $location) {
 
     $scope.statusList = [
         {
@@ -15,6 +15,9 @@ app.controller('TrabajadorController', function($scope, $http) {
     $scope.estado = 1;
     $scope.nroDoc = "";
     $scope.trabajadores = [];
+    $scope.consulta = true;
+    $scope.perfil = 1;
+    $scope.perfiles = [];
 
     var urlBasePath = "http://localhost:8081/DSD_Rest/rest/",
         reqTrabEstado = {
@@ -24,12 +27,29 @@ app.controller('TrabajadorController', function($scope, $http) {
                 dni: $scope.nroDoc,
                 estado: $scope.estado
             }
+        },
+        reqTrabAct = {
+            method: "POST",
+            url: urlBasePath + "trabajador/actualizar"
+        },
+        reqTrabReg = {
+            method: "POST",
+            url: urlBasePath + "trabajador/crear"
+        },
+        reqPerfil = {
+            method: "GET",
+            url: urlBasePath + "trabajador/perfil"
         };
 
     // Get default list
     $http(reqTrabEstado).success(function(response){
         $scope.trabajadores = response;
     }).error(function(response){
+        console.error("Se produjo un error");
+    });
+    $http(reqPerfil).success(function(response){
+        $scope.perfiles = response;
+    }).error(function(){
         console.error("Se produjo un error");
     });
 
@@ -45,4 +65,51 @@ app.controller('TrabajadorController', function($scope, $http) {
             console.error("Se produjo un error");
         });
     };
+
+    $scope.irEditar = function (trabajador){
+        $scope.consulta = false;
+        $scope.trabajador = trabajador;
+    }
+
+    $scope.regresar = function () {
+        $scope.consulta = true;
+    }
+    $scope.actualizar = function () {
+        if ($scope.trabajador && $scope.trabajador.nroDoc) {
+            reqTrabAct.data = $scope.trabajador;
+            $http(reqTrabAct).success(function(response){
+                alert("Se actualizó el trabajador");
+                $http(reqTrabEstado).success(function(response){
+                    $scope.trabajadores = response;
+                    $scope.regresar();
+                }).error(function(response){
+                    console.error("Se produjo un error");
+                });
+            }).error(function(){
+                console.error("Se produjo un error");
+            });
+        } else {
+            alert("Ingrese campos requeridos");
+        }
+    }
+    $scope.registrar = function () {
+        if ($scope.trabajador && $scope.trabajador.nroDoc) {
+            $scope.trabajador.perfil = $scope.perfil;
+            $scope.trabajador.estado = $scope.estado;
+            reqTrabReg.data = $scope.trabajador;
+            $http(reqTrabReg).success(function(response){
+                alert("Se creo el trabajador con el código: "+ response.codTrabajador);
+                $http(reqTrabEstado).success(function(response){
+                    $scope.trabajadores = response;
+                    $scope.regresar();
+                }).error(function(response){
+                    console.error("Se produjo un error");
+                });
+            }).error(function(){
+                console.error("Se produjo un error");
+            });
+        } else {
+            alert("Ingrese campos requeridos");
+        }
+    }
 });
--- DSD_Rest/src/com/upc/dsd/api/TrabajadorApiResource.java
+++ DSD_Rest/src/com/upc/dsd/api/TrabajadorApiResource.java
@@ -2,13 +2,17 @@ package com.upc.dsd.api;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.upc.dsd.bl.BussinesLogic;
 import com.upc.dsd.structures.Perfil;
+import com.upc.dsd.structures.Reserva;
 import com.upc.dsd.structures.Trabajador;
 import com.upc.dsd.structures.TrabajadorProyecto;
 
@@ -20,13 +24,14 @@ public class TrabajadorApiResource {
 	@Produces({ MediaType.APPLICATION_JSON })
 	public List<Trabajador> obtenerTrabajadoresxEstado(
 			@QueryParam("dni") final String dni,
-			@QueryParam("estado") final int estado) {
+			@QueryParam("estado") final int estado) throws WebApplicationException{
 
 		List<Trabajador> trabajadores = null;
 		try {
 			trabajadores = BussinesLogic.obtenerTrabajadoresxEstado(dni, estado);
 		} catch (Exception e) {
 			e.printStackTrace();
+			throw new WebApplicationException(404);
 		}
 
 		return trabajadores;
@@ -37,13 +42,34 @@ public class TrabajadorApiResource {
 	@Produces({ MediaType.APPLICATION_JSON })
 	public List<TrabajadorProyecto> obtenerTrabajadoresxProyecto(
 			@QueryParam("dni") final String dni,
-			@QueryParam("ruc") final String ruc) {
+			@QueryParam("ruc") final String ruc) throws WebApplicationException {
 
 		List<TrabajadorProyecto> trabajadores = null;
 		try {
 			trabajadores = BussinesLogic.obtenerTrabajadoresxProyecto(dni, ruc);
 		} catch (Exception e) {
 			e.printStackTrace();
+			throw new WebApplicationException(404);
+		}
+
+		return trabajadores;
+	}
+	
+	@GET
+	@Path("/fechas")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public List<Trabajador> obtenerTrabajadoresxFecha(
+			@QueryParam("fec_in") final String fec_in,
+			@QueryParam("fec_fin") final String fec_fin,
+			@QueryParam("perfil") final int perfil,
+			@QueryParam("estado") final int estado) throws WebApplicationException {
+
+		List<Trabajador> trabajadores = null;
+		try {
+			trabajadores = BussinesLogic.obtenerTrabajadoresxFecha(fec_in, fec_fin, perfil, estado);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new WebApplicationException(404);
 		}
 
 		return trabajadores;
@@ -52,15 +78,42 @@ public class TrabajadorApiResource {
 	@GET
 	@Path("/perfil")
 	@Produces({ MediaType.APPLICATION_JSON })
-	public List<Perfil> obtenerPerfiles() {
+	public List<Perfil> obtenerPerfiles() throws WebApplicationException {
 
 		List<Perfil> perfiles = null;
 		try {
 			perfiles = BussinesLogic.obtenerPerfiles();
 		} catch (Exception e) {
 			e.printStackTrace();
+			throw new WebApplicationException(404);
 		}
 
 		return perfiles;
+	}
+	
+	@POST
+	@Path("/actualizar")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public void actualizarTrabajador(Trabajador trabajador) throws WebApplicationException {
+		try {
+			BussinesLogic.actualizarTrabajador(trabajador);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new WebApplicationException(404);
+		}
+	}
+	@POST
+	@Path("/crear")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public Trabajador crearTrabajador(Trabajador trabajador) throws WebApplicationException {
+		
+		try {
+			trabajador = BussinesLogic.crearTrabajador(trabajador);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new WebApplicationException(404);
+		}
+		
+		return trabajador;
 	}
 }
